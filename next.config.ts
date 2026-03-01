401fb16402a481ccec093b5ee5c61d13cf024f16
yagi0328 <yagi.s.103755@gmail.com>
2026-03-01 16:08:10 +0900

    Vercelデプロイ修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    dirs: ["src/app", "src/components", "src/lib", "src/types"],
+  },
 };
 
 export default nextConfig;
