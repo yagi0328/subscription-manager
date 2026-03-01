49d9864c23325c42dd08819403a2ed3224cd8a95
yagi0328 <yagi.s.103755@gmail.com>
2026-03-01 17:02:33 +0900

    Vercel デプロイ設定修正

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   eslint: {
     dirs: ["src/app", "src/components", "src/lib", "src/types"],
   },
+  outputFileTracingIncludes: {
+    "/**": ["./src/generated/prisma/**"],
+  },
 };
 
 export default nextConfig;
